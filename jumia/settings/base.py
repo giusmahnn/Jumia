b3622fd2dfca6d7b6a873757d34bc175a4f5bcc1
Remigius Mgbeme <garethremigius@gmail.com>
2025-01-21 13:51:27 +0100

    settings updated

--- jumia/settings/base.py
+++ jumia/settings/base.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://jumiaclone.pythonanywhere.com/']
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
